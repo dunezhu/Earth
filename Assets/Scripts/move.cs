5ea552d65c362c09c7186eaa94b38e1fe96c32e3
Jun Zhu <123275291+dunezhu@users.noreply.github.com>
2023-08-13 16:16:18 -0700

    8/13/23
    Initiate

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 100;
+    [SerializeField] float movementSpeed = 500f;
+    [SerializeField] float turnSpeed = 30f;
 
     Transform myT;
 
-    private void Awake()
+    void Awake()
     {
         myT = transform;
     }
-    private void Update()
+
+    void Update()
     {
+        Turn();
         Thrust();
+    }
+
+    void Turn()
+    {
+        float yaw = turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float pitch = turnSpeed * Time.deltaTime * Input.GetAxis("Pitch");
+        float roll = turnSpeed * Time.deltaTime * Input.GetAxis("Roll");
+
+        myT.Rotate(-pitch, yaw, -roll);
     }
 
     void Thrust()
     {
-        myT.position += myT.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Fire1");
-        myT.position += myT.up * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
-        myT.position += myT.right * movementSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        if (Input.GetAxis("Vertical") > 0)
+            myT.position += myT.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
     }
 }
-
